2ae8b889174eb03742ba7050348997b9b767e88f
Elnura Amanturova <elnura.amantur@gmail.com>
2023-04-01 11:49:40 +0600

    realize in class InitDatabase.java

--- src/main/java/com/example/controlwork7/util/InitDatabase.java
+++ src/main/java/com/example/controlwork7/util/InitDatabase.java
@@ -1,6 +1,9 @@
 package com.example.controlwork7.util;
 
 import com.example.controlwork7.dao.ClientDao;
+import com.example.controlwork7.dao.DishDao;
+import com.example.controlwork7.dao.OrderDao;
+import com.example.controlwork7.dao.RestaurantDao;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,10 +11,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class InitDatabase {
     @Bean
-    CommandLineRunner init(ClientDao userDao) {
+    CommandLineRunner init(ClientDao clientDao, DishDao dishDao, OrderDao orderDao, RestaurantDao restaurantDao) {
         return (args) -> {
-//            userDao.createTable();
-//            taskDao.createTable();
+            clientDao.createTable();
+            dishDao.createTable();
+            orderDao.createTable();
+            restaurantDao.createTable();
+
         };
     }
 }
